f7449331361e0e3355a454ff9c8e7d4a59734198
Ko W <bohneundbean@gmail.com>
2020-10-11 18:16:17 +0900

    create Swift Project

---
+++ Global Pokemon Index/Global Pokemon Index/Global_Pokemon_IndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Global_Pokemon_IndexApp.swift
+//  Global Pokemon Index
+//
+//  Created by 渡辺 航 on 2020/10/11.
+//
+
+import SwiftUI
+
+@main
+struct Global_Pokemon_IndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
