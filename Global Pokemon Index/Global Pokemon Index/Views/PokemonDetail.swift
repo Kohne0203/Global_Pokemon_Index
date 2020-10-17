365b569e50500f417a3d711eda0e85ea6519fe99
Ko W <bohneundbean@gmail.com>
2020-10-17 19:10:56 +0900

    [add]PokemonDetailView
    ポケモン詳細画面のviewを作成

---
+++ Global Pokemon Index/Global Pokemon Index/Views/PokemonDetail.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Global Pokemon Index
+//
+//  Created by Ko.W on 2020/10/11.
+//
+
+import SwiftUI
+
+struct PokemonDetail: View {
+    var body: some View {
+        HStack {
+            PokemonImage(image: Image("025"))
+            VStack(alignment: .leading, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
+                Text("ピカチュウ")
+                    .font(.largeTitle)
+                    .frame(alignment: .leading)
+                    .padding(.bottom)
+                Text("タイプ：電気")
+                Text("分類："+"ねずみポケモン")
+            })
+        }
+        Spacer()
+    }
+}
+
+struct PokemonDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetail()
+    }
+}
